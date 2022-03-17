eb36cb57c4ae154885f3b7374ac68c5780ac0ff6
RavindraWiguna <ravindrawiguna@gmail.com>
2022-03-18 01:23:01 +0700

    add prototype

--- prototype_control.py
+++ prototype_control.py
@@ -5,21 +5,28 @@ import pyttsx3 as speech
 from mycv2 import createBoxWithText
 import numpy as np
 
+def findMiddlePoint(point1, point2, width, height):
+    return (int((point1.x+point2.x)/2*width), int((point1.y+point2.y)/2*height),)
+
+
 def main():
     # mediapipe stuff
     mp_drawing = mp.solutions.drawing_utils # helper to draw
-    mp_hand = mp.solutions.hands # all hand utils
-    hand_tracker = mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands = 2) 
+    mp_drawing_styles = mp.solutions.drawing_styles #also a helper, but for style B)
+    mp_hands = mp.solutions.hands # all hand utils
+    hand_tracker = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands = 2) 
     # open cv cam stuff
-    camera = SoeroCam(width=640, height=480,isMirrored=True) # camera for video input
+    cameraDrone = SoeroCam(width=1440, height=1080,isMirrored=False, src="/dev/video2") # camera for video input
+    # print(cameraDrone.getOriginalCamDim())
+    cameraWeb = SoeroCam(width=800, height=600, isMirrored=True, src=0)
     close_key = ord('q') # what key to press to exit
     
     #speech stuff
     engine = speech.init()
 
     # units
-    fontScale = camera.width/960
-    unit_x = camera.width//32
+    fontScale = cameraDrone.width/960
+    unit_x = cameraDrone.width//32
     mid_x = unit_x*16
     
     # rectangle ui sutff
@@ -32,10 +39,8 @@ def main():
     left_rect_x = (mid_x - lrw)//2
     right_rect_x = mid_x + (mid_x - rrw)//2
 
-    black_screen = np.zeros((camera.height, camera.width, 3), np.uint8)
-
     # loop for each frame captured
-    for img in camera.infiniteCapture():
+    for img in cameraWeb.infiniteCapture():
         # convert img from BGR to RGB because mediapipe takes RGB image
         # and opencv default is BGR
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -47,18 +52,39 @@ def main():
         # Detect!
         results = hand_tracker.process(img)
 
-        # makes img writable
-        img.flags.writeable = True
+        # makes img writable [noneed]
+        # img.flags.writeable = True
 
         # convert back to BGR
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
+        # get fpv frame
+        fpvFrame = cameraDrone.singleCapture()
+
+        #add rect
+        fpvFrame[0: lrh, left_rect_x: left_rect_x +lrw] = left_hand_rect
+        fpvFrame[0:rrh, right_rect_x: right_rect_x + rrw] = right_hand_rect
+        # print(cv2.getTextSize("LEFT HAND", cv2.FONT_HERSHEY_SIMPLEX, fontScale, 2))
+        cv2.line(fpvFrame,(mid_x,0),(mid_x,cameraDrone.height),(255,0,0),3)
+        # fpvFrame = cv2.resize(fpvFrame, (1440, 1080))
+        cv2.circle(fpvFrame,(mid_x//2,cameraDrone.height//2), 32, (0,0,255), -1)
+        
         # draw the results!
         total_hand = 0
+        thumbTip = []
+        indexTip = []
         if (results.multi_hand_landmarks):
             for hand in results.multi_hand_landmarks:#a list of hand landmark
-                mp_drawing.draw_landmarks(black_screen, hand, mp_hand.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(
+                    fpvFrame, 
+                    hand, 
+                    mp_hands.HAND_CONNECTIONS) 
+                    # mp_drawing_styles.get_default_hand_landmarks_style(),
+                    # mp_drawing_styles.get_default_hand_connections_style())
                 total_hand+=1
+                landMark = hand.landmark
+                indexTip.append(landMark[mp_hands.HandLandmark.INDEX_FINGER_TIP])
+                thumbTip.append(landMark[mp_hands.HandLandmark.THUMB_TIP])
                 # print(f'{len(hand.landmark)} and type: {type(hand.landmark)}')
                 # print("======1=====")
          
@@ -66,17 +92,21 @@ def main():
             # engine.say(f'LOST {2-total_hand} HAND')
             # engine.runAndWait() #need threader
             
-        #add rect
-        img[0: lrh, left_rect_x: left_rect_x +lrw] = left_hand_rect
-        img[0:rrh, right_rect_x: right_rect_x + rrw] = right_hand_rect
-        # print(cv2.getTextSize("LEFT HAND", cv2.FONT_HERSHEY_SIMPLEX, fontScale, 2))
-        cv2.line(img,(mid_x,0),(mid_x,camera.height),(255,0,0),3)
-        cv2.imshow('HandTracking', img)
-        cv2.imshow('AR ceritanya', black_screen)
-        black_screen = np.zeros((camera.height, camera.width, 3), np.uint8)
+        
+        midPoints = []
+        for i in range(total_hand):
+            midPoints.append(findMiddlePoint(thumbTip[i], indexTip[i], cameraDrone.width, cameraDrone.height))
+        
+        for point in midPoints:
+            # print(type(point[0]))
+            print(point)
+            cv2.line(fpvFrame, (mid_x//2,cameraDrone.height//2), point, (255, 128, 56), 3)
+        cv2.imshow('HandTracking', fpvFrame)
+        # cv2.imshow('aaa', img)
         key = cv2.waitKey(1) & 0xFF
         if key == close_key:
-            camera.stopCapture()
+            cameraDrone.stopCapture()
+            cameraWeb.stopCapture()
             break
 
 
